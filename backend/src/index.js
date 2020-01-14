0fc9f06a34a890d5c72cd6cbd16d58253a889f86
Débora Rodrigues <deborarsousa@gmail.com>
2020-01-14 13:19:39 -0300

    Backend do projeto omnistack

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+//Tipos Parametros
+//Query Params: req.query(Filtros,ordenação, paginação...)
+//Route Params: req.params(Identificar um recurso na alteração ou remoção)
+//Body: req.body(Dados para criação ou alteração de um registro)
+
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/omnistack10", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
