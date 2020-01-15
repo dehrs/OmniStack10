1d93da25030b266bd7356001a552042811669613
Débora Rodrigues <deborarsousa@gmail.com>
2020-01-15 16:03:56 -0300

    Estruturação do front-end

---
+++ web/src/App.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+function App() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Cadastar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="d"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar Perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="d"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar Perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="d"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar Perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
+                alt="d"
+              />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </p>
+            <a href="https://github.com/diego3g">Acessar Perfil no GitHub</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
